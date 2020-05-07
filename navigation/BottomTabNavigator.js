6c8026d60c3626734b0ba3894164e400117cf66d
AJDP <jduplooy050@gmail.com>
2020-05-07 10:59:20 +0200

    update: tab header style

--- navigation/BottomTabNavigator.js
+++ navigation/BottomTabNavigator.js
@@ -1,59 +1,69 @@
-import * as React from 'react';
-import { Platform, } from 'react-native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import TabBarIcon from '../components/TabBarIcon';
-import { Entypo } from '@expo/vector-icons';
-import HomeScreen from '../screens/HomeScreen';
-import InfoScreen from '../screens/InfoScreen';
+import * as React from "react";
+import { Platform } from "react-native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import TabBarIcon from "../components/TabBarIcon";
+import Colors from "../constants/Colors";
+import HomeScreen from "../screens/HomeScreen";
+import InfoScreen from "../screens/InfoScreen";
 
 const BottomTab = createBottomTabNavigator();
-const INITIAL_ROUTE_NAME = 'Home';
+const INITIAL_ROUTE_NAME = "Home";
 
 export default function BottomTabNavigator({ navigation, route }) {
-  // Set the header title on the parent stack navigator depending on the
-  // currently active tab. Learn more in the documentation:
-  // https://reactnavigation.org/docs/en/screen-options-resolution.html
-  navigation.setOptions({ headerTitle: getHeaderTitle(route) });
+  navigation.setOptions({
+    headerTitle: getHeaderTitle(route),
+    headerTitleAlign: 'center',
+    headerStyle: {
+      backgroundColor: Colors.tabIconSelected,
+    },
+    headerTintColor: "white",
+  });
 
-  let options = {}
+  let options = {};
 
-  if (Platform.OS === 'ios') {
+  if (Platform.OS === "ios") {
     options = {
       style: {
         paddingTop: 10,
         paddingBottom: 25,
       },
-    }
+    };
   } else {
     options = {
       style: {
+        height: 60,
         paddingTop: 10,
       },
       labelStyle: {
         marginTop: 5,
+        marginBottom: 5,
       },
-    }
+    };
   }
 
   return (
-    <BottomTab.Navigator initialRouteName={INITIAL_ROUTE_NAME}
-      tabBarOptions={options}>
-
+    <BottomTab.Navigator
+      initialRouteName={INITIAL_ROUTE_NAME}
+      tabBarOptions={options}
+    >
       <BottomTab.Screen
         name="Home"
         component={HomeScreen}
         options={{
-          title: 'Home',
-          tabBarIcon: ({ focused }) => <TabBarIcon focused={focused} itype="ion" name="ios-list" />,
+          title: "Home",
+          tabBarIcon: ({ focused }) => (
+            <TabBarIcon focused={focused} itype="ion" name="ios-list" />
+          ),
         }}
       />
       <BottomTab.Screen
         name="Info"
         component={InfoScreen}
         options={{
-          title: 'Info',
-          tabBarIcon: ({ focused }) => 
-            <TabBarIcon focused={focused} itype="en" name="info" />,
+          title: "Info",
+          tabBarIcon: ({ focused }) => (
+            <TabBarIcon focused={focused} itype="en" name="info" />
+          ),
         }}
       />
     </BottomTab.Navigator>
@@ -61,12 +71,13 @@ export default function BottomTabNavigator({ navigation, route }) {
 }
 
 function getHeaderTitle(route) {
-  const routeName = route.state?.routes[route.state.index]?.name ?? INITIAL_ROUTE_NAME;
+  const routeName =
+    route.state?.routes[route.state.index]?.name ?? INITIAL_ROUTE_NAME;
 
   switch (routeName) {
-    case 'Home':
-      return 'Home';
-    case 'Info':
-      return 'Level Info';
+    case "Home":
+      return "Home";
+    case "Info":
+      return "Level Info";
   }
 }
